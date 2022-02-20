509c98aa7132c29361816c30dfd0b726d5874a40
Salahuddin Shaikh <33448178+salahoddin88@users.noreply.github.com>
2022-02-20 11:54:24 +0530

    Product Admin

--- product/admin.py
+++ product/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from product.models import ProductCategory
+from product.models import ProductCategory, Product
 
 
 admin.site.register(ProductCategory)
+admin.site.register(Product)
